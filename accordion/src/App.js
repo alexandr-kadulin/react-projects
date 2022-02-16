b55b1c3d702a71ccb05d44ffe1db9453791db330
Aleksandr Kadulin <alexandr-kadulin@hotmail.com>
2022-02-16 16:59:56 +0200

    update accordion

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 
 function App() {
-	const [questions, setQuestions] = useState(data);
-	return (
-		<main>
-			<div className="container">
-				<h3>questions and answers about greatest football legends in history</h3>
-				<section className="info">
-					{questions.map((question) => {
-						return (
-							<SingleQuestion key={question.id} {...question} />
-						);
-					})}
-				</section>
-			</div>
-		</main>
-	);
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
