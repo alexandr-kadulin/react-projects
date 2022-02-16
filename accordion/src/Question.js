ce8f997c124004a9e1bf6990bb60fa4dc2f290fa
Aleksandr Kadulin <alexandr-kadulin@hotmail.com>
2022-02-16 16:59:56 +0200

    initial commit

--- accordion/src/Question.js
+++ accordion/src/Question.js
@@ -1,20 +1,23 @@
-import React, { useState } from 'react';
-import { BsPlusCircleFill } from 'react-icons/bs';
-import { HiMinusCircle } from 'react-icons/hi';
-
+import React, { useState } from "react";
+import { BsPlusCircleFill } from "react-icons/bs";
+import { HiMinusCircle } from "react-icons/hi";
 const Question = ({ title, info }) => {
-	const [showInfo, setShowInfo] = useState(false);
-	return (
-		<article className="question">
-			<header>
-				<h4>{title}</h4>
-				<button className='btn' onClick={() => setShowInfo(!showInfo)}>
-					{showInfo ? <HiMinusCircle className='react-icons' /> : <BsPlusCircleFill className='react-icons' />}
-				</button>
-			</header>
-			{showInfo && <p>{info}</p>}
-		</article>
-	);
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? (
+            <HiMinusCircle className="question-icon" />
+          ) : (
+            <BsPlusCircleFill className="question-icon" />
+          )}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
--- accordion/src/data.js
+++ accordion/src/data.js
@@ -1,34 +1,33 @@
 const questions = [
-	{
-		id: 1,
-		title: 'Who is Cristiano Ronaldo?',
-		info:
-			'Cristiano Ronaldo dos Santos Aveiro (born 5 February 1985), better known as Cristiano Ronaldo, or by his nickname "CR7", is a Portuguese professional footballer who plays as a forward. He plays for Serie A club Juventus and is the captain of the Portuguese national team.',
-	},
-	{
-		id: 2,
-		title: 'Who is Lionel Messi?',
-		info:
-			'Also known as Leo Messi, (born 24 June 1987) is an Argentine professional footballer who plays as a forward and captains both Spanish club Barcelona and the Argentina national team. ... He has scored over 750 senior career goals for club and country, and has the most goals ever by a player for a single club.',
-	},
-	{
-		id: 3,
-		title: 'Who is Diego Maradona?',
-		info:
-			'Diego Maradona, in full Diego Armando Maradona, (born October 30, 1960, Lanus, Buenos Aires, Argentina—died November 25, 2020, Tigre, Buenos Aires), Argentine football (soccer) player who is generally regarded as the top footballer of the 1980s and one of the greatest of all time.',
-	},
-	{
-		id: 4,
-		title: 'Who is Pelé?',
-		info:
-			'Pelé, byname of Edson Arantes do Nascimento, (born October 23, 1940, Três Corações, Brazil), Brazilian football (soccer) player, in his time probably the most famous and possibly the best-paid athlete in the world.',
-	},
-	{
-		id: 5,
-		title: 'Who is Ronaldinho?',
-		info:
-			'Ronaldo de Assis Moreira (born 21 March 1980), commonly known as Ronaldinho Gaúcho (Brazilian Portuguese: [ʁonawˈdʒĩɲu gaˈuʃu]) or simply Ronaldinho, is a Brazilian former professional footballer and current ambassador for Barcelona. He played mostly as an attacking midfielder, but was also deployed as a winger.',
-	},
-];
-
-export default questions;
+  {
+    id: 1,
+    title: 'Do I have to allow the use of cookies?',
+    info:
+      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+  },
+  {
+    id: 2,
+    title: 'How do I change my My Page password?',
+    info:
+      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+  },
+  {
+    id: 3,
+    title: 'What is BankID?',
+    info:
+      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+  },
+  {
+    id: 4,
+    title: 'Whose birth number can I use?',
+    info:
+      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+  },
+  {
+    id: 5,
+    title: 'When do I recieve a password ordered by letter?',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+  },
+]
+export default questions
