89c7b3f6e8e06abc9a6bead12d46df3b21030743
Aleksandr Kadulin <alexandr-kadulin@hotmail.com>
2022-02-18 07:01:36 +0200

    update slider

--- slider/src/data.js
+++ slider/src/data.js
@@ -1,40 +1,40 @@
 const people = [
-	{
-		id: 1,
-		image:
-			'https://images.ctfassets.net/usf1vwtuqyxm/w0my38UcZEISkqSe26IAO/c8ed26ea32ae308be1cf68b0511ee599/AlbusDumbledore_WB_F6_DumbledoreHoldingElderWandToHisForehead_Still_100615_Land.jpg?w=914',
-		name: 'Albus Dumbledore',
-		title: 'Professor',
-		quote:
-			'Differences of habit and language are nothing at all if our aims are identical and our hearts are open.',
-	},
-	{
-		id: 2,
-		image:
-			'https://d.newsweek.com/en/full/301392/voldemort.jpg?w=1600&h=1600&q=88&f=448a2f79833ef3d618b7a440c4ea5671',
-		name: 'Lord Voldemort',
-		title: 'The Dark Lord',
-		quote:
-			'There is no good and evil, there is only power and those too weak to seek it.',
-	},
-	{
-		id: 3,
-		image:
-			'https://thepopinsider.com/wp-content/uploads/2019/04/WarnerBros_HarryPotter.jpg',
-		name: 'Harry Potter',
-		title: 'The Boy Who Lived',
-		quote:
-			"I won't blast people out of my way just because they're there. That's Voldemort's job.",
-	},
-	{
-		id: 4,
-		image:
-			'https://images.ctfassets.net/usf1vwtuqyxm/6miaYuD2i4wemokq8Q02Uo/ba922817407a667349ae64c8e334d3b6/Dobby_WB_F2_DobbyClickingFingersCastingSpell_Still_080615_Land.jpg',
-		name: 'Dobby',
-		title: 'Free elf',
-		quote:
-			'Dobby has heard of your greatness, sir, but of your goodness, Dobby never knew...',
-	},
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "susan andersen",
+    title: "the boss",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+  },
 ];
 
 export default people;
