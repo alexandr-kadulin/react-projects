ba2ae5ce27508c38de9a8dc744bfd3e0e04a430c
alexandr-kadulin <alexandr-kadulin@hotmail.com>
2024-06-20 09:50:04 +0300

    update structure

--- birthday-reminder/src/__tests__/App.test.js
+++ birthday-reminder/src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import App from "../App";
+import { App } from "../components";
 
 it("should render the amount of birthdays", () => {
   render(<App />);
--- birthday-reminder/src/__tests__/List.test.js
+++ birthday-reminder/src/__tests__/List.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import List from "../List";
 import data from "../data";
+import { List } from "../components";
 
 it("should render the list", () => {
   render(<List people={data} />);
